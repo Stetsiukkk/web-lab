0ebcdded9eca292c446a5127d677d4fbb5c05967
Pavlo-St <pavlo.stetsiuk.kn.2021@lpnu.ua>
2024-10-23 12:42:04 +0300

    img full fix #1

--- react-app/src/pages/Index.js
+++ react-app/src/pages/Index.js
@@ -18,35 +18,35 @@ function Index() {
                             </div>
                         </li>
                         <li>
-                            <img src="ASSETS/images/image2.jpg" width="300" height="200" />
+                            <img src="/assets/images/image2.jpg" width="300" height="200" />
                             <div className="caption">
                                 <h3>Аудиторія 2</h3>
                                 <p>Велика аудиторія на 150 людей. У адутиторії розміщена велика дошка та трибуна для виступів. </p>
                             </div>
                         </li>
                         <li>
-                            <img src="ASSETS/images/image3.jpg" width="300" height="200" />
+                            <img src="/assets/images/image3.jpg" width="300" height="200" />
                             <div className="caption">
                                 <h3>Аудиторія 3</h3>
                                 <p>Невелика аудиторія на 36 людей. У аудиторії наявні стільчики зі столами, а також стіл, трибуна, дошка та проектор для викладача.</p>
                             </div>
                         </li>
                         <li>
-                            <img src="ASSETS/images/image4.jpg" width="300" height="200" />
+                            <img src="/assets/images/image4.jpg" width="300" height="200" />
                             <div className="caption">
                                 <h3>Аудиторія 4</h3>
                                 <p>Дуже велика аудиторія на 500 місць. Наявний мікрофон з колонками для кращого звуку, та велика сцена. Також є місця на балконах на 2 поверсі.</p>
                             </div>
                         </li>
                         <li>
-                            <img src="ASSETS/images/image5.jpg" width="300" height="200" />
+                            <img src="/assets/images/image5.jpg" width="300" height="200" />
                             <div className="caption">
                                 <h3>Аудиторія 5</h3>
                                 <p>Середня аудиторія для 60 людей. Наявний прямокутний стіл та відкрита зона у кімнаті. </p>
                             </div>
                         </li>
                         <li>
-                            <img src="ASSETS/images/image6.jpg" width="300" height="200" />
+                            <img src="/assets/images/image6.jpg" width="300" height="200" />
                             <div className="caption">
                                 <h3>Аудиторія 6 </h3>
                                 <p>Невелика аудиторія на 40 людей. Наявна дошка, карта світу, проектор.</p>
